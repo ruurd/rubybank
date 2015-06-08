97319d5dfc355e0ff35f8825b06fa08e06cef212
Ruurd Pels <rfpels@gmail.com>
2015-06-08 11:37:31 +0200

    IBAN should be unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605210613) do
+ActiveRecord::Schema.define(version: 20150608093525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20150605210613) do
     t.datetime "created_at",                                          null: false
     t.datetime "updated_at",                                          null: false
   end
+
+  add_index "accounts", ["iban"], name: "index_accounts_on_iban", unique: true, using: :btree
 
   create_table "mutations", force: :cascade do |t|
     t.integer  "account_id",                                  null: false
