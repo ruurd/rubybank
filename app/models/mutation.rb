d2a9923c5a75b05a410c9698ebfcd24e96f295d6
Ruurd Pels <rfpels@gmail.com>
2015-06-08 15:52:32 +0200

    Argh. Mutation cannot be < 0. Stupid.

--- app/models/mutation.rb
+++ app/models/mutation.rb
@@ -2,12 +2,15 @@ class Mutation < ActiveRecord::Base
   belongs_to :account
   belongs_to :from_account, class_name: Account
 
-  monetize :amount_cents
+  monetize :amount_cents_cents, :allow_nil => false, :numericality => {
+                                  :greater_than_or_equal_to => 0
+                              }
+
 
   # OK this $%^&*( sucks because I think this is
   # PostgreSQL specific.
-  scope :payment, -> {where('amount_cents < 0')}
-  scope :reception, -> {where('amount_cents > 0')}
+  scope :payment, -> { where('amount_cents < 0') }
+  scope :reception, -> { where('amount_cents > 0') }
 
   # Make answers searchable
   def self.search(search)
